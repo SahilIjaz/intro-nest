9cf2f24598c5670671cefd36f5a36154a550e22d
Sahil Ijaz <hssahil2913@gmail.com>
2025-07-26 11:57:06 +0500

    dtos used by third party

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,6 +1,15 @@
+import { IsEmail, isEmpty, IsNumber, IsString } from 'class-validator';
+
 export class createUser {
+  @IsString({ message: 'Id needs to be a string value.' })
   id: number;
+
+  @IsString({ message: 'Name shoud be in string.' })
   name: string;
+
+  @IsNumber()
   age: number;
+
+  @IsEmail()
   email: string;
 }
